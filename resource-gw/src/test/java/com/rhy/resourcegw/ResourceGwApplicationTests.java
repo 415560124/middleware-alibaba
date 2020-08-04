be4052e51397daca858e10c5c57400e9ab78bb3a
Rhy <415560124@qq.com>
2020-08-04 17:02:10 +0800

    提交人：任鸿毅 提交标题：项目搭建 提交内容： 1.网关搭建 2.资源服务器搭建

---
+++ resource-gw/src/test/java/com/rhy/resourcegw/ResourceGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhy.resourcegw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceGwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
