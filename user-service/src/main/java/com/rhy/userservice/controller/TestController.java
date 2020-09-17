8508fd7eab84cb41dfbe2da04f6207d0b07a29b4
Rhy <415560124@qq.com>
2020-09-17 18:38:28 +0800

    提交人：任鸿毅 提交标题：alibaba组件框架开发示例 提交内容： 1.user-service测试模块 2.nacos注册中心

---
+++ user-service/src/main/java/com/rhy/userservice/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.rhy.userservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Rhy
+ * @title 测试控制器
+ * @description 测试控制器
+ * @createTime 2020年08月07日 16:17:00
+ * @modifier：Rhy
+ * @modification_time：2020-08-07 16:17
+ */
+@RestController
+public class TestController {
+
+}
